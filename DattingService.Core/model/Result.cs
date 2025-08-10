9e5f56bea0f08878e9cacc23a0b12a0d3bd143a6
sergxlove <sergxlove@mail.ru>
2025-08-10 12:21:01 +0600

    add new models

---
+++ DattingService.Core/model/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace DattingService.Core.model
+{
+    public class Result<T>
+    {
+        public T Value { get; }
+        public string Error { get; }
+        public bool IsSuccess
+        {
+            get
+            {
+                if (Error == string.Empty) return true;
+                return false;
+            }
+        }
+
+        private Result(T value, string error)
+        {
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(value, string.Empty);
+
+        public static Result<T> Failure(string error) => new Result<T>(default!, error);
+    }
+}
